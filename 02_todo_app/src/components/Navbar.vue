0838154543fdcdd876c0bef638cc4c56f7949755
ny-Sopheaktra <60089537+ny-Sopheaktra@users.noreply.github.com>
2020-12-29 13:50:50 +0700

    todo_app

---
+++ 02_todo_app/src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav style="color: green">
+    <div class="nav-wrapper" style="color: green">
+      <div class="container" style="color: green">
+        <router-link to="/" class="brand-logo" style="color: yellow">Todo App</router-link>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li>
+            <router-link to="/" style="color: yellow">Home</router-link>
+          </li>
+          <li>
+            <router-link to="/about" style="color: yellow">Account</router-link>
+          </li>
+          <li>
+            <li>
+            <router-link to="/about" style="color: yellow">News</router-link>
+          </li>
+          <li>
+            <router-link to="/about" style="color: yellow">Blogs</router-link>
+          </li>
+          <li>
+            <router-link to="/about" style="color: yellow">Contact</router-link>
+          </li>
+          <li>
+            <router-link to="/about" style="color: yellow">About</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
